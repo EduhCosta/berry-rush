adec45b1344bd7bb390e260afbbdea07c2564bed
Eduardo Costa <eduhcosta@live.com>
2023-04-06 20:20:08 -0300

    Finishing Cutscene mockScreen

---
+++ Assets/Scripts/GameObject/CutsceneJumper.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class CutsceneJumper : MonoBehaviour
+{
+    [Header("NextSceneProperties")]
+    [SerializeField] public string SceneName;
+
+    [Header("Joystick Inputs")]
+    [SerializeField] public InputActionAsset Actions;
+
+    void Awake()
+    {
+        Actions.FindActionMap("Gameplay").FindAction("JumpCutscene").performed += GoToScene;
+    }
+
+    public void OnEnable()
+    {
+        Actions.FindActionMap("Gameplay").Enable();
+    }
+
+    public void OnDisable()
+    {
+        Actions.FindActionMap("Gameplay").Disable();
+    }
+
+    void GoToScene(InputAction.CallbackContext context)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
