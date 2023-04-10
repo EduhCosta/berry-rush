4447b7f3352a2b5efa7005b05262545dfd970103
Eduardo Costa <eduhcosta@live.com>
2023-04-10 13:37:08 -0300

    Finish feature

---
+++ Assets/Scripts/GameObject/BoostTrack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostTrack : MonoBehaviour
+{
+    [Tooltip("Value to add velocity to kart")]
+    [SerializeField] float BoostValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (PlayerIdentifier.IsPlayer(other))
+        {
+            SphereCartController cart = PlayerIdentifier.GetKart(other);
+            cart.OnBoost(BoostValue);
+        }
+
+        if (AIIdentifier.IsAI(other))
+        {
+            AICartController cart = AIIdentifier.GetAIKart(other);
+            cart.OnBoost(BoostValue);
+        }
+    }
+}
