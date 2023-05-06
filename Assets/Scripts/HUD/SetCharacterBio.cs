da632206e1d65b9ba188a7dd8da0e656d992051b
RenatoPodesta <renato.podesta@outlook.com>
2023-05-06 15:06:55 -0300

    AddBio

---
+++ Assets/Scripts/HUD/SetCharacterBio.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class SetCharacterBio : MonoBehaviour
+{
+    [SerializeField] public TMP_Text TxtBio;
+
+    public void OnEnable()
+    {
+        SelectableButton.SelectedCharacter += SetBio;
+    }
+
+    public void OnDisable()
+    {
+        SelectableButton.SelectedCharacter -= SetBio;
+    }
+
+    void SetBio(string CharacterName, string Bio, Sprite Icon)
+    {
+        TxtBio.text = Bio;
+    }
+}
